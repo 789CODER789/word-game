23fbd01b9993feac7ca42c473ff4e77d6755bf72
789CODER789 <isaxodjayevf@gmail.com>
2024-12-09 15:28:59 +0500

    word game in c++

--- so'z o'yini/word.cpp
+++ so'z o'yini/word.cpp
@@ -2,9 +2,11 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
+#include <cctype>
+#include <string>
 using namespace std;
 
-string words[] = {"xodim", "xayol", "xat", "xarajat", "xizmat", "xohish", "xulosa", "xarid", "xabar"};
+string words[] = {"xodim", "xayol", "xat", "xarajat", "xizmat", "xohish", "xulosa", "xarid", "xabar", "ahamiyat", "ahli", "ayol", "aholi", "alhamdulilloh", "alohidalik", "hol", "bahamjihat", "behafsala", "ehson", "ehtiros", "ehtiyot", "hadeb", "hadis", "hajviya"};
 
 string toLowerCase(string str){
     transform(str.begin(), str.end(), str.begin(), ::tolower);
@@ -16,16 +18,21 @@ int main(){
     char yanaOynaysizmi;
     
     do{
-        string word = words[rand() % 9];
+        int wordCount = sizeof(words) / sizeof(words[0]);
+        string word = words[rand() % wordCount];
         string guess;
         int lives = 3;
 
+        string text = word;
+        int textLength = text.length();
+
         cout << "SO'Z O'YINI\n";
         cout << "SHARTLAR: Men taxmin qilgan so'zni topishga harakat qiling!\n";
-        cout << "YORDAM: Bu so'z x harifidan boshlanadi.\n";
+        cout << "YORDAM: Bu so'z "<< word[0] <<" harifidan boshlanadi va so'z "<< textLength << " ta harifdan iborat.\n";
         cout << "Sizni 3ta urunishingiz bor. Omad!\n";
+        bool gameOver = false;
 
-        while (lives > 0){
+        while (lives > 0 && !gameOver){
             cout << "So'z kiriting: ";
             cin >> guess;
             guess = toLowerCase(guess);
@@ -33,7 +40,7 @@ int main(){
 
             if(guess == word){
                 cout << "Qoyil, Bu to'g'ri so'z! Toptingiz!\n";
-                break;
+                gameOver = true;
             }
             else{
                 lives--;
@@ -47,6 +54,7 @@ int main(){
 
         cout << "Yana o'ynaysizmi? (h/y)\n";
         cin>> yanaOynaysizmi;
+        yanaOynaysizmi = toLowerCase(string(1, yanaOynaysizmi))[0];
 
     } while (yanaOynaysizmi == 'H' || yanaOynaysizmi == 'h');
 
